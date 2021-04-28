218083600e7163620c15c7552cd0f8c3f9b8ca28
dread557 <dadaolawunmi09@gmail.com>
2021-04-28 18:46:12 +0100

    tried mobile nav but failed:/

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
+let mobileNav = document.querySelector(".mobile-nav");
+let openNav = document.querySelector(".open-nav");
 
-
+openNav.addEventListener("click", ()=>{
+    mobileNav.getElementsByClassName.display = "block"
+});
