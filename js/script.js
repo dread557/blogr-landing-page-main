9f9f38eecc5057b6fbaa81866d71e132c217534b
dread557 <dadaolawunmi09@gmail.com>
2021-04-28 18:48:19 +0100

    Revert "tried mobile nav but failed:/"
    This reverts commit 218083600e7163620c15c7552cd0f8c3f9b8ca28.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,2 @@
-let mobileNav = document.querySelector(".mobile-nav");
-let openNav = document.querySelector(".open-nav");
 
-openNav.addEventListener("click", ()=>{
-    mobileNav.getElementsByClassName.display = "block"
-});
+
