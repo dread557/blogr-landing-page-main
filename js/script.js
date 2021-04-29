3edc00bd52c818bdfb71db4250e3458363edd80f
dread557 <dadaolawunmi09@gmail.com>
2021-04-29 21:41:39 +0100

    styled mobile nav and added javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,50 @@
+const accBtns = document.querySelectorAll(".accordion-header");
+const accContents = document.querySelectorAll(".accordion-body");
+const openNav = document.querySelector(".open-nav");
+const closeNav = document.querySelector(".close-nav");
+const mobileNav = document.querySelector(".mobile-nav");
 
 
+
+accBtns.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+
+        accContents.forEach((acc) => {
+            if(e.target.nextElementSibling !== acc && acc.classList.contains("active")){
+                acc.classList.remove("active");
+                accBtns.forEach(btn => btn.classList.remove("active"));
+            }
+        });
+
+        const panel = btn.nextElementSibling;
+        panel.classList.toggle("active");
+        btn.classList.toggle("active");
+    });
+});
+
+window.onclick = (e) => {
+    if(!e.target.matches(".accordion-header")){
+        accBtns.forEach(btn => btn.classList.remove("active"));
+        accContents.forEach(acc => acc.classList.remove("active"));
+    }
+}
+
+openNav.addEventListener("click", ()=>{
+    mobileNav.style.display = "block";
+    if(mobileNav.style.display = "block") {
+        closeNav.style.display = "block";
+        openNav.style.display = "none";
+    } else {
+        closeNav.style.display = "none";
+    }
+});
+
+closeNav.addEventListener("click", ()=>{
+    mobileNav.style.display = "none";
+    if(mobileNav.style.display = "none") {
+        closeNav.style.display = "none";
+        openNav.style.display = "block";
+    } else {
+        closeNav.style.display = "none";
+    }
+});
